d1dbbe1fd6fe6cbcb6b4d7e92c0d55f61224e031
こけっち <50144466+sim1222@users.noreply.github.com>
2021-11-02 02:03:10 +0900

    Update androidbuildsetup.sh

--- androidbuildsetup.sh
+++ androidbuildsetup.sh
@@ -2,29 +2,21 @@
 echo "Android build setup sh for ubuntu 20.04"
 echo -e "/n"
 
-echo "Download platformtools..."
-wget https://dl.google.com/android/repository/platform-tools-latest-linux.zip
-unzip platform-tools-latest-linux.zip -d ~
-{
-echo "# add Android SDK platform tools to path"
-echo "if [ -d "$HOME/platform-tools" ] ; then"
-echo "    PATH="$HOME/platform-tools:$PATH""
-echo "fi"
-} >> ~/.profile
-source ~/.profile
-
 echo "Installing some packages...(and delete old jdk...)"
 sudo apt update
-sudo apt-get purge openjdk-* icedtea-* icedtea6-* -y
-sudo apt install bison g++-multilib git gperf libxml2-utils make zlib1g-dev:i386 zip liblz4-tool libncurses5 libssl-dev bc flex openjdk-11-jdk python ccache -y
-sudo apt-get install bison g++-multilib git gperf libxml2-utils make zlib1g-dev:i386 zip liblz4-tool libncurses5 libssl-dev bc flex -y
-sudo apt install bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-gtk3-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev python
-java -version
+sudo apt install git
+cd ~/
+git clone https://github.com/akhilnarang/scripts
+cd scripts
+./setup/android_build_env.sh
 
 cd ~/
 git clone https://github.com/akhilnarang/scripts
 cd scripts
 ./setup/android_build_env.sh
+
+sudo apt install -y git-core gnupg flex bison build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 libncurses5 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev libxml2-utils xsltproc unzip fontconfig
+sudo apt install -y bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5 libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-gtk3-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev 
 
 
 echo "Setting ccache..."
@@ -44,6 +36,6 @@ rm /swap.img
 fallocate -l 16G /swap.img
 mkswap /swap.img
 swapon /swap.img
-souece ~/.bashrc
+source ~/.bashrc
 
 echo "done. Copy build.sh to android dir and run."
